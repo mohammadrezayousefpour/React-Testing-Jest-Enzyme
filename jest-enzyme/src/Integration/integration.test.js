f164e292daf57972a136640aaab3f617a79349ec
unknown <mohammad0reza79@gmail.com>
2022-05-07 11:45:11 +0430

    redux thunk is adding2

--- jest-enzyme/src/Integration/integration.test.js
+++ jest-enzyme/src/Integration/integration.test.js
@@ -3,7 +3,28 @@ import { guessWord } from "../Actions/index";
 
 describe("guessWord action dispatcher ", () => {
   describe("no guessed words", () => {
-    test("update state correctly for unsuccessful guess", () => {});
+    let Store;
+    const secretWord = "party";
+    const guessedWord = "train";
+    const initialState = { secretWord };
+    beforeEach(() => {
+      Store = StoreFactory(initialState);
+    });
+    test("update state correctly for unsuccessful guess", () => {
+      Store.dispatch(guessWord(guessedWord));
+      const newState = Store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [
+          {
+            guessWord: guessedWord,
+            letterMatch: 3,
+          },
+        ],
+      };
+      expect(newState).toEqual(expectedState);
+    });
     test("update state correctly for successful guess", () => {});
   });
   describe("some guessed words", () => {
